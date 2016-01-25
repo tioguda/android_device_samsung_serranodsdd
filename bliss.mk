91a1fa5e4412ca87e6d5c6464a83e91c0c0ab573
tioguda <tioguda@users.noreply.github.com>
2016-01-25 03:10:09 -0200

    Update and rename cm.mk to bliss.mk

--- bliss.mk
+++ bliss.mk
@@ -1,5 +1,5 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/serranodsdd/full_serranodsdd.mk)
 
@@ -10,4 +10,4 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranodsxx TARGET_DEVICE=serranods BUILD_FINGERPRINT="samsung/serranodsxx/serranods:4.4.2/KOT49H/I9192XXUCNG2:user/release-keys" PRIVATE_BUILD_DESC="serranodsxx-user 4.4.2 KOT49H I9192XXUCNG2 release-keys"
 
 PRODUCT_DEVICE := serranodsdd
-PRODUCT_NAME := cm_serranodsdd
+PRODUCT_NAME := bliss_serranodsdd
